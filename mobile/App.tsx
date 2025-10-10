1bfce6fb4988d6ee323f4c7f4e21393b3f242a9d
Kimberly Lancaster <kim.lancaster@protonmail.com>
2025-10-10 12:28:18 -0700

    Add session loading on app start for persistence testing

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { store } from './src/store/store';
+import { setSessions } from './src/store/sessionSlice';
+import { SessionService } from './src/services/SessionService';
 import { ConnectionScreen } from './src/screens/ConnectionScreen';
 import { TerminalScreen } from './src/screens/TerminalScreen';
 
 const Stack = createStackNavigator();
 
+const AppContent = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const loadSessions = async () => {
+      const sessions = await SessionService.getSessions();
+      dispatch(setSessions(sessions));
+    };
+    loadSessions();
+  }, [dispatch]);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Connection" component={ConnectionScreen} />
+        <Stack.Screen name="Terminal" component={TerminalScreen} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+};
+
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Connection" component={ConnectionScreen} />
-          <Stack.Screen name="Terminal" component={TerminalScreen} />
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </NavigationContainer>
+      <AppContent />
     </Provider>
   );
 }
